5a7768cc60fbda28f4f7188e74324a5798f526a9
Kevin Wittek <kevin.wittek@styracosoft.de>
2017-02-25 19:04:15 +0100

    Add environment configurable bean

--- src/main/java/io/devops/gathering/bootiful/Greeter.java
+++ src/main/java/io/devops/gathering/bootiful/Greeter.java
@@ -1,10 +1,14 @@
 package io.devops.gathering.bootiful;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Greeter implements CommandLineRunner {
+
+    @Value("${greeter.name}")
+    private String name;
 
     @Override
     public void run(String... args) throws Exception {
@@ -13,7 +17,7 @@ public class Greeter implements CommandLineRunner {
 
     private void greet() {
         System.out.println("##################");
-        System.out.println("Hello World!");
+        System.out.printf("Hello %s!\n", name);
         System.out.println("##################");
     }
 }
